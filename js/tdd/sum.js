e4637bb786fcb3330bb2c7a7cb9dcafe99db10e7
Morgan Benton <morgan.benton@gmail.com>
2018-02-26 10:54:38 -0500

    sum refactor complete

--- js/tdd/sum.js
+++ js/tdd/sum.js
@@ -1,11 +1,26 @@
 /**
  * A function to add two numbers
- * @param [number] a The first number to be added
- * @param [number] b The second number to be added
- * @return [number]  The sum of a and b
+ * @param  {Number} a The first number to be added
+ * @param  {Number} b The second number to be added
+ * @return {Number}   The sum of a and b
  */
-function sum(a, b) {
+let sum = (a,b) => {
   return a + b;
-}
+};
 
 module.exports = sum;
+
+// Alternative ways to write the
+// sum function
+// 
+// function sum(a, b) {
+//   return a + b;
+// }
+// 
+// let sum = function(a,b) {
+//   return a + b;
+// };
+// 
+// let sum = (a,b) => a + b;
+// 
+// module.exports = (a,b) => a + b;
