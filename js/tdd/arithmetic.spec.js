0a4ed3bcc88ab70630584de557bacceedeae7fe1
Morgan Benton <morgan.benton@gmail.com>
2018-03-14 10:35:35 -0400

    added difference to js tdd

--- js/tdd/arithmetic.spec.js
+++ js/tdd/arithmetic.spec.js
@@ -1,5 +1,6 @@
 // import code to be tested
 const sum = require('./sum');
+const difference = require('./difference');
 
 // specify what the library does
 describe("An arithmetic library", () => {
@@ -9,6 +10,10 @@ describe("An arithmetic library", () => {
     expect(sum(8,9)).toBe(17);
     expect(sum(-3,4)).toBe(1);
     expect(sum(304,4123)).toBe(4427);
+  });
+
+  it("can subtract two numbers", () => {
+    expect(difference(5,3)).toBe(2);
   });
 
 });
