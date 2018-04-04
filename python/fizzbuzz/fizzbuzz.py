63393e351961f5404ba2b593d89be08762d3b151
Morgan Benton <morgan.benton@gmail.com>
2018-04-04 11:02:20 -0400

    progress on python fizzbuzz

--- python/fizzbuzz/fizzbuzz.py
+++ python/fizzbuzz/fizzbuzz.py
@@ -2,10 +2,10 @@
 
 class FizzBuzz:
 
-  fizz = lambda self, x: "Fizz" if x % 3 == 0 else x
+  fizz  = lambda self, x: "Fizz"     if x %  3 == 0 else x
+  buzz  = lambda self, x: "Buzz"     if x %  5 == 0 else x
+  fibu  = lambda self, x: "FizzBuzz" if x % 15 == 0 else x
+  count = lambda self, x: list(range(1, x + 1))
 
-  # def fizz(self, x):
-  #   return "Fizz" if x % 3 == 0 else x
-
-  def buzz(self, x):
-    return "Buzz" if x % 5 == 0 else x
+  def play(self, x):
+    return list(map(lambda y: self.buzz(self.fizz(self.fibu(y))), self.count(x)))
--- python/fizzbuzz/test_fizzbuzz.py
+++ python/fizzbuzz/test_fizzbuzz.py
@@ -24,3 +24,24 @@ def test_buzz():
   assert fb.buzz(1) == 1
   assert fb.buzz(3) == 3
   assert fb.buzz(37) == 37
+
+def test_fibu():
+  assert fb.fibu(15) == "FizzBuzz"
+  assert fb.fibu(45) == "FizzBuzz"
+  assert fb.fibu(165) == "FizzBuzz"
+  assert fb.fibu(1) == 1
+  assert fb.fibu(3) == 3
+  assert fb.fibu(37) == 37
+
+def test_count():
+  assert fb.count(5)  == [ 1, 2, 3, 4, 5 ]
+  assert fb.count(10) == [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+
+def test_play():
+  assert fb.play(5)  == [ 1, 2, "Fizz", 4, "Buzz" ]
+  assert fb.play(20) == [
+    1, 2, "Fizz", 4, "Buzz",
+    "Fizz", 7, 8, "Fizz", "Buzz",
+    11, "Fizz", 13, 14, "FizzBuzz",
+    16, 17, "Fizz", 19, "Buzz"
+  ]
